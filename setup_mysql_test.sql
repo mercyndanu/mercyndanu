352030f1cb0824a68bbefb4430c40edde99c38a1
mercyndanu <40683767+mercyndanu@users.noreply.github.com>
2023-06-21 23:12:37 +0300

    Add files via upload

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Script that creates hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
